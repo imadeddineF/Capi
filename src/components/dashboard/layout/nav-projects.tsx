92fb7d8185e3ff57135edfece6e93071a46b0d6e
imadeddineF <imadeddinefillalipro@gmail.com>
2025-06-30 13:24:16 +0100

    test

--- src/components/dashboard/layout/nav-projects.tsx
+++ src/components/dashboard/layout/nav-projects.tsx
@@ -2,48 +2,48 @@
 
 import { type LucideIcon } from "lucide-react";
 import {
-	SidebarGroup,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	// useSidebar,
+    SidebarGroup,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    // useSidebar,
 } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 import { Link } from "next-view-transitions";
 
 export function NavProjects({
-	projects,
+    projects,
 }: {
-	projects: {
-		name: string;
-		url: string;
-		icon: LucideIcon;
-	}[];
+    projects: {
+        name: string;
+        url: string;
+        icon: LucideIcon;
+    }[];
 }) {
-	// const { isMobile } = useSidebar();
-	const pathname = usePathname();
+    // const { isMobile } = useSidebar();
+    const pathname = usePathname();
 
-	return (
-		<SidebarGroup>
-			<SidebarMenu>
-				{projects.map((item) => (
-					<SidebarMenuItem key={item.name} className="h-10">
-						<SidebarMenuButton
-							asChild
-							className={
-								pathname === item.url
-									? "bg-primary text-white hover:text-white hover:bg-primary" // Active tab styling
-									: "" // Inactive tab styling
-							}
-						>
-							<Link href={item.url}>
-								<item.icon />
-								<span>{item.name}</span>
-							</Link>
-						</SidebarMenuButton>
-					</SidebarMenuItem>
-				))}
-			</SidebarMenu>
-		</SidebarGroup>
-	);
+    return (
+        <SidebarGroup>
+            <SidebarMenu>
+                {projects.map((item) => (
+                    <SidebarMenuItem key={item.name} className="h-10">
+                        <SidebarMenuButton
+                            asChild
+                            className={
+                                pathname === item.url
+                                    ? "bg-primary text-white hover:text-white hover:bg-primary" // Active tab styling
+                                    : "" // Inactive tab styling
+                            }
+                        >
+                            <Link href={item.url}>
+                                <item.icon />
+                                <span>{item.name}</span>
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
 }
